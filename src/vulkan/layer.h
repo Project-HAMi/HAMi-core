d609ac9c4ab797e44c0c0a069b8d88d75cd3a15b
Jea-Eok-Kim <je.kim@xiilab.com>
2026-04-21 23:34:46 +0900

    fix(vulkan): fallback VK_LAYER_EXPORT macro for Vulkan-Headers 1.3.280+
    Signed-off-by: Jea-Eok-Kim <je.kim@xiilab.com>

--- src/vulkan/layer.h
+++ src/vulkan/layer.h
@@ -4,6 +4,16 @@
 #include <vulkan/vulkan.h>
 #include <vulkan/vk_layer.h>
 
+/* Vulkan-Headers 1.3.280+ dropped VK_LAYER_EXPORT. Default visibility on
+ * ELF/Mach-O is sufficient; Windows would need __declspec(dllexport). */
+#ifndef VK_LAYER_EXPORT
+#  if defined(_WIN32)
+#    define VK_LAYER_EXPORT __declspec(dllexport)
+#  else
+#    define VK_LAYER_EXPORT __attribute__((visibility("default")))
+#  endif
+#endif
+
 #ifdef __cplusplus
 extern "C" {
 #endif
